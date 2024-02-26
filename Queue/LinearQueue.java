8b0ac30713354180c69a111d8d880e6aa51c42d6
MaleeshaAluwihare <159033744+MaleeshaAluwihare@users.noreply.github.com>
2024-02-26 08:10:25 +0530

    LinearQueueUpdated

--- Queue/LinearQueue.java
+++ Queue/LinearQueue.java
@@ -1,68 +1,68 @@
-public class Queue{
+public class LinearQueue {
 
-    private int front;
-    private int rear;
-    private int NumCount;
-    private int[] QueueArray;
-    private int maxSize;
-
-    //Constructor
-    public Queue(int size){
-
-        front = 0;
-        rear = -1;
-        NumCount = 0;
-        maxSize = size;
-        QueueArray = new int[maxSize];
-    }
-
-    //insert method
-    pubic void insert(int number){
-
-        if(rear == maxSize - 1){
-            System.out.println("Queue is full");
+        private int front;
+        private int rear;
+        private int NumCount;
+        private int[] QueueArray;
+        private int maxSize;
+    
+        //Constructor
+        public LinearQueue(int size){
+    
+            front = 0;
+            rear = -1;
+            NumCount = 0;
+            maxSize = size;
+            QueueArray = new int[maxSize];
         }
-        else{
-            NumCount++;
-            QueueArray[++rear];
-
-            // rear++;
-            // QueueArray[rear] = number;
+    
+        //insert method
+        public void insert(int number){
+    
+            if(rear == maxSize - 1){
+                System.out.println("Queue is full");
+            }
+            else{
+                NumCount++;
+                QueueArray[++rear];
+    
+                // rear++;
+                // QueueArray[rear] = number;
+            }
         }
-    }
-
-    //remove method
-    public int remove(){
-
-        if(NumCount == 0){
-            return -99     //Check if Queue is empty
+    
+        //remove method
+        public int remove(){
+    
+            if(NumCount == 0){
+                return -99;     //Check if Queue is empty
+            }
+            else{
+                NumCount--;
+                return QueueArray[front++];
+    
+                // int x = QueueArray[front];
+                // front++;
+                // return x;
+            }
         }
-        else{
-            NumCount--;
-            return QueueArray[front++];
-
-            // int x = QueueArray[front];
-            // front++;
-            // return x;
+    
+        //peekfront method
+        public int peekFront(){
+    
+            if(NumCount == 0){
+                return -99;
+            }
+            else{
+                return QueueArray[front];
+            }
         }
-    }
-
-    //peekfront method
-    public int peekFront(){
-
-        if(NumCount == 0){
-            return -99;
+    
+        public boolean isEmpty(){
+            return (NumCount == 0);
         }
-        else{
-            return QueueArray[front];
+    
+        public boolean isFull(){
+            return (NumCount == maxSize);
         }
-    }
-
-    public boolean isEmpty(){
-        return (NumCount == 0);
-    }
-
-    public boolean isFull(){
-        return (NumCount == maxSize);
-    }
 }
