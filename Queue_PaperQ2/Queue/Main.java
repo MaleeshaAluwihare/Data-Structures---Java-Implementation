7ddd7d070007a8f39f39742383aa63e386d78ebf
MaleeshaAluwihare <159033744+MaleeshaAluwihare@users.noreply.github.com>
2024-08-13 10:15:56 +0530

    Create Main.java

---
+++ Queue_PaperQ2/Queue/Main.java
@@ -0,0 +1,17 @@
+package Queue;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter max size:");
+        int size = sc.nextInt();
+
+        QueueX Q1 = new QueueX(size);
+
+          
+    }
+    
+}
